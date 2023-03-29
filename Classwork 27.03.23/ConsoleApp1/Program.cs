7f5f3b99d607dcdfc82f9417d64cea1a25b1723d
NikSi039 <sidorova9999@internet.ru>
2023-03-29 21:12:33 +0200

    classwork 29.03.23

--- Classwork 27.03.23/ConsoleApp1/Program.cs
+++ Classwork 27.03.23/ConsoleApp1/Program.cs
@@ -8,39 +8,65 @@ namespace ConsoleApp1
         {
             string _directory = "";
             string _fileName = "Data.txt";
+            bool _isCommand = false;
+            bool _needExit = false;
             string _fullPath = "";
             List <string> _data = new List<string>();
+            string[] _commandsList = new string[]
+            {
+                "exit","remove","help","?"
+            };
             ReadFile();
 
-            while (true)
+            while (!_needExit)
             {
                 PrintData();
                 string line = Console.ReadLine();
-                if (line == "exit")
+                if (CheckCommands(line))
                 {
-                    Console.WriteLine("Файл сохранён");
-                    break;
+                    ExecuteCommand(line);
                 }
-                if (line == "remove")
+                else
                 {
-                    Console.WriteLine("Какую строку удаляем? Аааа? (номер от нуля пж)");
-                    int lineNumber = int.Parse(Console.ReadLine());
-                    _data.RemoveAt(lineNumber);
-                    continue;
+                    AppendData(line);
+                    WriteDataToFile();
                 }
-                AppendData(line);
-                
-                Console.Clear();
             }
-
-            WriteDataToFile();
             Console.ReadLine();
+            //End of program
             List <string> ReadFile()
             {
                 _directory = Directory.GetCurrentDirectory();//Путь до exe-файла
                 _directory = new DirectoryInfo(_directory).Parent.Parent.Parent.FullName; //Перейти по каталогу на 3 уровня вверх с помощью parent
                 _fullPath = Path.Combine(_directory, _fileName); // Комбинирование пути +папка или +файл
                 return _data = File.ReadAllLines(_fullPath).ToList(); // Чтение файла и запись string в переменную
+            }
+            void ExecuteCommand(string command)
+            {
+                if (command == "exit")
+                {
+                    _needExit = true;
+                }
+                if (command == "remove")
+                {
+                    Console.WriteLine("Какую строку удаляем?");
+                    int lineNumber = int.Parse(Console.ReadLine());
+                    try
+                    {
+                        _data.RemoveAt(lineNumber);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Ошибка, попробуйте ещё раз: ");
+                    }
+                }
+                WriteDataToFile();
+
+            }
+            bool CheckCommands(string line)
+            {
+                line = line.Trim().ToLower();
+                return _commandsList.Contains(line);
             }
             void AppendData(string data)
             {
@@ -52,8 +78,12 @@ namespace ConsoleApp1
             }
             void PrintData()
             {
+                int i = 0;
                 Console.WriteLine("DATA: ");
-                Console.WriteLine(string.Join(Environment.NewLine, _data));
+                foreach (string note in _data)
+                {
+                    Console.WriteLine($"{i++}. {note}");
+                }
                 Console.WriteLine("-------------------------------------");
             }
         }
---
+++ Classwork 29.03.23/Classwork 29.03.23/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Classwork_29._03._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
