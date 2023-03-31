131af3d1113e9183bd060939cfea3895d13115f0
NikSi039 <sidorova9999@internet.ru>
2023-03-31 21:21:46 +0200

    Classwork 31.03.2023

---
+++ Classwork 31.03.23/Classwork2/Classwork2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Classwork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] _array = new int[10];
+            int[] _array2 = new int[5];
+            int[] _array3 = new int[3];
+            _array = FillArray(_array.Length);
+            _array2 = FillArray(_array2.Length);
+            _array3 = FillArray(_array3.Length);
+            Console.WriteLine(Join(_array));
+            Console.WriteLine(Join(_array2));
+            Console.WriteLine(Join(_array3));
+            Console.WriteLine($"Сумма 1 массива: {ArraySum(_array)}");
+            Console.WriteLine($"Сумма 2 массива: {ArraySum(_array2)}");
+            Console.WriteLine($"Сумма 3 массива: {ArraySum(_array3)}");
+            Console.ReadLine();
+            //END
+            int ArraySum(int[] array)
+            {
+                int result = 0;
+
+                foreach (var item in array)
+                {
+                    result += item;
+                }
+                return result;
+            }
+            int[] FillArray(int arrayLength)
+            {
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    _array[i] = new Random().Next(0, 100);
+                }
+                return _array;
+            }
+            string Join(int[] array)
+            {
+                string result = "";
+                foreach (int item in array)
+                {
+                    result += item + " - ";
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ Classwork 31.03.23/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+
+namespace Classwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime finishDateTime = new DateTime(2023, 3, 31, 21, 20, 0);
+            DateTime _diffDateTime;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"День: {GetDays()}\nМесяц: {GetMonth()}\nГод: {GetYears()}");
+                Console.WriteLine($"Время: {GetHours()}:{GetMinutes(DateTime.Now)}:{GetSeconds(DateTime.Now)}");
+                _diffDateTime = new DateTime(finishDateTime.Ticks - DateTime.Now.Ticks);
+                Console.WriteLine($"Время до конца пары: {GetMinutes(_diffDateTime)}: {GetSeconds(_diffDateTime)}");
+                Thread.Sleep(1000);
+            }
+            Console.ReadLine();
+            //END
+            int GetSeconds(DateTime dateTimeParam)
+            {
+                return dateTimeParam.Second;
+            }
+            int GetMinutes(DateTime dateTimeParam)
+            {
+                return dateTimeParam.Minute;
+            }
+            int GetHours()
+            {
+                return DateTime.Now.Hour;
+            }
+            int GetDays()
+            {
+                return DateTime.Now.Day;
+            }
+            int GetMonth()
+            {
+                return DateTime.Now.Month;
+            }
+            int GetYears()
+            {
+                return DateTime.Now.Year;
+            }
+        }
+    }
+}
