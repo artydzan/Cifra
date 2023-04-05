48c3bba5f4e89020ce4094dd5a90a7db90c33e0c
artydzan <arthur20002012@gmail.com>
2023-04-05 21:17:10 +0200

    cringe

---
+++ Classwork 05.04.2023/Classwork 05.04.2023/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Classwork_05._04._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string oper = "";
+            bool _success = true;
+            line = line.Replace(oper, " " + oper + " ");
+            value1 = TryParse(_items[0], ref _success);
+            oper = _items[1].Trim();
+            value2 = TryParse(_items[2], ref _success);
+
+
+            float TryParse(string line, ref bool success)
+            {
+                float result = 0f;
+                try
+                {
+                    result = float.Parse(line);
+                }
+                catch
+                {
+                    success = false;
+                }
+                return result;
+            }
+
+            List<string> Getitems(string text)
+            {
+                List<string> items = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+                foreach (var item in items)
+                {
+                    if (item == "")
+                    {
+                        items.RemoveAt(item);
+                    }
+                }
+            }
+            float value1 = 0;
+            float value2 = 0;
+            string oper = "";
+            float.TryParse(items[0], out value1);
+            oper = items[1].Trim();
+            float.TryParse(items[2], out value2);
+        }
+    }
+}
