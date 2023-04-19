78ca7dbddf39a9c4148b8f484c6b97b119a70b74
Valeriy <128506624+SnazzyK@users.noreply.github.com>
2023-04-19 22:15:02 +0200

    ok

--- Classwork 19.04.23/Classwork/Program.cs
+++ Classwork 19.04.23/Classwork/Program.cs
@@ -27,7 +27,7 @@
             //        Console.WriteLine(i+"");
             //    Console.WriteLine();
             Console.WriteLine("Введите размер массива");
-            int n = int.Parse(args[0]);
+            int n = int.Parse(Console.ReadLine());
             int[] mas = new int[n];
             Random r = new Random();
             int count = 0;
@@ -37,13 +37,32 @@
                 bool b = false;
                 for (int i = 0; i<=count; i++)
                 {
-                    if (mas[i] == a) b = true;
-                    if(b == false)
-                    {
-                        mas[count] = a;
-                        count++;
-                    }   
+                    if (mas[i]==a) b=true;
                 }
+                if (b == false)
+                {
+                    mas[count] = a;
+                    count++;
+                }
+                foreach (int i in mas) Console.WriteLine(i + " ");
+                Console.WriteLine();
+                int max = mas[0], min = mas[0];
+                for (int i = 0; i <= mas.Length; i++)
+                {
+                    if (mas[i] > max) max = mas[i];
+                    if (mas[i] < min) min = mas[i];
+                }
+                int[] result = new int[n - 2];
+                int j = 0;
+                for (int i = 0; i< n; i++)
+                {
+                    if (mas[i] != max && mas[i]!=min)
+                    {
+                        result[j++] = mas[i];
+                    }
+                }
+                foreach (int i in result) Console.WriteLine(i + " ");
+                Console.WriteLine();
             }
         }
     }
---
+++ Classwork 19.04.23/new Classwork/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введте размер массива:");
+int n = int.Parse(Console.ReadLine());
+int[] mas = new int[n];
+Random r = new Random();
+int index = -1;
+bool b = false;
+for (int i = 0; i < n; i++)
+{
+    mas[i] = r.Next(-10, 11);
+    Console.WriteLine(mas[i] + " ");
+    if (b !=true && mas[i]<0)
+    {
+        b = true;
+        index = i;
+    }
+}
+Console.WriteLine();
+Console.WriteLine(index);
+Console.WriteLine("Введите число");
+int m = int.Parse(Console.ReadLine());
+int[] result = new int[n + 1];
+b = false;
+for (int i = 0; i < result.Length; i++)
+{
+    if (i == index)
+    {
+        result[i] = mas[i];
+        result[++i] = m;
+        b = true;
+    }
+    else
+    {
+        result[i] = mas[i - 1];
+    }
+}
+foreach (int i in result) Console.WriteLine(i+" ");
+Console.WriteLine();
