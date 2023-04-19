76f4533b5c4275fb9706aeea7e1b809f1400b89c
Valeriy <128506624+SnazzyK@users.noreply.github.com>
2023-04-19 21:44:35 +0200

    required

---
+++ Classwork 19.04.23/Classwork 19.04.23/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Classwork_19._04._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] mas = new double[12];
+            Random r =new Random();
+            //float S = 0;
+            for (int i = 1; i < mas.Length; i++)
+            {
+                mas[i] = r.Next(10, 100);
+                Console.Write($"{mas[i]:F2} + ");
+                //S += mas[i];
+            }
+            //Console.WriteLine();
+            //Console.WriteLine("S="+S);
+            //Console.WriteLine($"S = {S:F2}");
+            //Console.WriteLine("S={0}," ,S);
+            //int[] score = new int[10];
+            //Random r = new Random();
+            //int summ = 0;
+            //for (int i = 0; i < score.Length; i++)
+            //{
+            //    score[i] = r.Next(1, 10);
+            //    Console.WriteLine(score[i]);
+            //    summ = summ + score[i];
+            //}
+            //Console.WriteLine($"Сумма = {summ}");
+            //if (summ > 40)
+            //{
+            //    Console.WriteLine("Прошёл, сумма баллов выше 40");
+            //}
+            //int a = 5, b = 2;
+            //int c = a;
+            //a = b;
+            //b = c;
+            double temp = mas[2];
+            mas[2] = mas[5];
+            mas[5] = temp;
+            foreach (double i in mas) Console.WriteLine(i);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Classwork 19.04.23/Classwork/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Classwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //{
+            //    int[] mass = new int[12];
+            //    Random r = new Random();
+            //    int min = mass[0];
+            //    for (int i = 0; i < mass.Length; i++)
+            //    {
+            //        mass[i] = r.Next(10, 100);
+            //        Console.Write($"{mass[i]}");
+            //        if (mass[i] < min) min = mass[i];
+            //    }
+            //    Console.WriteLine();
+            //    int index = -1;
+            //    for (int i = 0; i < mass.Length; i++)
+            //    {
+            //        if (mass[i] == min) index = i;
+            //    }
+            //    int temp = mass[0];
+            //    mass[0] = mass[index];
+            //    mass[index] = temp;
+            //    foreach (double i in mass)
+            //        Console.WriteLine(i+"");
+            //    Console.WriteLine();
+            Console.WriteLine("Введите размер массива");
+            int n = int.Parse(args[0]);
+            int[] mas = new int[n];
+            Random r = new Random();
+            int count = 0;
+            while(count<n)
+            {
+                int a=r.Next(-10,11);
+                bool b = false;
+                for (int i = 0; i<=count; i++)
+                {
+                    if (mas[i] == a) b = true;
+                    if(b == false)
+                    {
+                        mas[count] = a;
+                        count++;
+                    }   
+                }
+            }
+        }
+    }
+}
